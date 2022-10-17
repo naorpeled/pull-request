e574a31aea5fd79eab0b9499108b61301f4d28ec
Naor Peled <thehecticbyte@gmail.com>
2022-10-17 22:24:27 +0300

    wip

--- packages/issue-finder/src/main.ts
+++ packages/issue-finder/src/main.ts
@@ -1,1 +1,47 @@
-console.log('Hello World!');
+import { graphql as GithubGraphQLClient } from '@octokit/graphql';
+
+(async () => {
+  const response = await GithubGraphQLClient(`
+  {
+    search(
+      query: "is:open label:good-first-issue -label:duplicate,wontfix -linked:pr created:>2022-10-01 comments:<10 no:assignee"
+      type: ISSUE
+      first: 100
+    ) {
+      issueCount
+      pageInfo {
+        endCursor
+        startCursor
+      }
+      edges {
+        node {
+          ... on Issue {
+            number
+            title
+            body
+            lastEditedAt
+            labels(first: 100) {
+              edges {
+                node {
+                  name
+                }
+              }
+            }
+            repository {
+              url
+              stargazerCount
+              languages(first: 100) {
+                nodes {
+                  name
+                }
+              }
+              forkCount
+            }
+          }
+        }
+      }
+    }
+  }
+`);
+
+})();
