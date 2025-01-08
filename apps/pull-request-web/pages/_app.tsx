081a5c8269b91e6c56e9672398b8eed93fee7375
Naor Peled <thehecticbyte@gmail.com>
2025-01-08 22:50:41 +0200

    feat(apps/web): add Tailwind

--- apps/pull-request-web/pages/_app.tsx
+++ apps/pull-request-web/pages/_app.tsx
@@ -6,7 +6,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to pull-request-web!</title>
+        <title>Welcome to pull-request.dev!</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
